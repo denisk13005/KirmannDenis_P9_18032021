f64c265d4c5daf033633a85e4e192af12cc3e944
kirmann <k.dkle@hotmail.fr>
2022-01-08 15:02:53 +0100

    commentaire Bills.js

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -4,22 +4,24 @@ import Logout from "./Logout.js"
 
 export default class {
   constructor({ document, onNavigate, firestore, localStorage }) {
-    this.document = document
-    this.onNavigate = onNavigate
+    this.document = document //le body
+    this.onNavigate = onNavigate//méthode onNavigate du router
     this.firestore = firestore
+    //sélection du bouton nouvelle note de frais et déclenchement de la méthode handleClickNewBill au click
     const buttonNewBill = document.querySelector(`button[data-testid="btn-new-bill"]`)
     if (buttonNewBill) buttonNewBill.addEventListener('click', this.handleClickNewBill)
+    //sélection des icones oeils et déclenchement de la méthode handleClickIconEye au click
     const iconEye = document.querySelectorAll(`div[data-testid="icon-eye"]`)
     if (iconEye) iconEye.forEach(icon => {
       icon.addEventListener('click', (e) => this.handleClickIconEye(icon))
     })
     new Logout({ document, localStorage, onNavigate })
   }
-
+  // envoi sur la page newBill
   handleClickNewBill = e => {
     this.onNavigate(ROUTES_PATH['NewBill'])
   }
-
+  //ouvre la modale d'affichage de la photo justificative de la note de frais
   handleClickIconEye = (icon) => {
     const billUrl = icon.getAttribute("data-bill-url")
     const imgWidth = Math.floor($('#modaleFile').width() * 0.5)
