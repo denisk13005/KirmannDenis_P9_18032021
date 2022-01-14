635df194e72b4ce534f4baebd11b4d9e4408d19f
kirmann <k.dkle@hotmail.fr>
2022-01-14 21:53:27 +0100

    commentaire router.js

--- src/app/Router.js
+++ src/app/Router.js
@@ -11,22 +11,23 @@ import { ROUTES, ROUTES_PATH } from "../constants/routes.js"
 
 export default () => {
   const rootDiv = document.getElementById('root') // point d'entrée html
-  rootDiv.innerHTML = ROUTES({ pathname: window.location.pathname }) // génére la page en fonction de l'url
+  rootDiv.innerHTML = ROUTES({ pathname: window.location.pathname }) // génére la page en fonction de l'url et l'injecte dans l'url
+
   //crée une méthode onNavigate avec comme paramètre le pathname
- 
   window.onNavigate = (pathname) => {
     // ajoute à l'historique du navigateur 
     window.history.pushState(
       {},
       pathname,
       window.location.origin + pathname
-    )
-    if (pathname === ROUTES_PATH['Login']) { //si le pathname est '/'
+      )
+      //si le pathname est '/' envoi au login
+      if (pathname === ROUTES_PATH['Login']) { 
       rootDiv.innerHTML = ROUTES({ pathname })
       document.body.style.backgroundColor="#0E5AE5"
       new Login({ document, localStorage, onNavigate, PREVIOUS_LOCATION, firestore })
     } 
-    
+    //sinon envoie à la page correspondante
     else if (pathname === ROUTES_PATH['Bills']) {
       rootDiv.innerHTML = ROUTES({ pathname, loading: true })
       const divIcon1 = document.getElementById('layout-icon1')
@@ -78,12 +79,13 @@ export default () => {
       onNavigate(PREVIOUS_LOCATION)
     }
   }
-
+  // quand on a cliqué sur le bouton retour de la souris vérifie l'url 
+  //si pas de hash renvoi au login
   if (window.location.pathname === "/" && window.location.hash === "") {
     new Login({ document, localStorage, onNavigate, PREVIOUS_LOCATION, firestore })
     document.body.style.backgroundColor="#0E5AE5"
   }
-  
+  // sinon renvoi à la page correspondante
   else if (window.location.hash !== "") {
     if (window.location.hash === ROUTES_PATH['Bills']) {
       rootDiv.innerHTML = ROUTES({ pathname: window.location.hash, loading: true })
